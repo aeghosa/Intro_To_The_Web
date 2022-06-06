0c0d863e073ad87b24b065eb822822f612d70e54
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-06-06 16:44:36 +0100

    Walkthrough – Sinatra: Returning HTML

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
 require 'sinatra'
-# require "sinatra/reloader" if development?
+require "sinatra/reloader" if development?
 
-get '/' do
-  'hello!'
-end
-
-get '/secret' do
-  'This is a secret page'
+get '/cat' do
+  "<div>
+    <img src='https://i.imgur.com/jFaSxym.png'>
+   </div>"
 end
