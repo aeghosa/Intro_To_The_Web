750ca3471cc98d7706e00d53a867afff82845aa6
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-06-06 17:17:42 +0100

    Walkthrough – Sinatra: Views

--- app.rb
+++ app.rb
@@ -2,7 +2,5 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='https://i.imgur.com/jFaSxym.png'>
-   </div>"
+  erb(:index)
 end
